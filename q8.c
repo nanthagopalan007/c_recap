1951e41d421940f31478967578c791d84101c04e
Nanthagopalan <nanthagopalan07@gmail.com>
2025-08-13 08:37:02 +0530

    project1

---
+++ q8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("This is the first statement.\n");
+
+    // Print a blank line
+    printf("\n");
+
+    printf("This is the second statement.\n");
+
+    return 0;
+}
