be98dbf6d70d9800747f33c61e66113878e91575
Nanthagopalan <nanthagopalan07@gmail.com>
2025-08-13 22:09:06 +0530

    project1

---
+++ q31.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    int a = 20, b = 5;
+    int result = a / b;
+    printf("Result: %d\n", result);
+    return 0;
+}
---
+++ q32.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+    if (b != 0)
+        printf("Quotient: %d\n", a / b);
+    else
+        printf("Error: Division by zero.\n");
+    return 0;
+}
